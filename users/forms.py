03b64d9de45b9a59f686684dd263addb8be2187d
sl.ars <sl.ars@icloud.com>
2025-02-26 18:10:26 +0500

    Init React Frontend

--- users/forms.py
+++ users/forms.py
@@ -2,31 +2,36 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from users.models import User
 
+
 class RegisterForm(UserCreationForm):
-    """ Registration Form with Django validation """
+    """ Registration Form with Django validation, including phone number and avatar """
+
     role = forms.ChoiceField(choices=[
         ('customer', 'Customer'),
         ('trader', 'Trader'),
-        ('sales', 'Sales Representative')
-    ])
+        ('sales_rep', 'Sales Representative')
+    ], widget=forms.Select(attrs={'class': 'form-control'}))
+
+    phone_number = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone number'})
+    )
+
+    avatar = forms.ImageField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2', 'role']
+        fields = ['username', 'email', 'phone_number', 'avatar', 'password1', 'password2', 'role']
         widgets = {
-            'username': forms.TextInput(attrs={'class': 'form-control'}),
-            'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
-            'role': forms.Select(attrs={'class': 'form-control'}),
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter email'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}),
         }
+
 
 class LoginForm(AuthenticationForm):
     """ Custom Login Form with Bootstrap styling """
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
-    def __init__(self, *args, **kwargs):
-        super(LoginForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'placeholder': 'Enter username'})
-        self.fields['password'].widget.attrs.update({'placeholder': 'Enter password'})
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}))
